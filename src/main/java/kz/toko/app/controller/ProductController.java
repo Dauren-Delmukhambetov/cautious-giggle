217ab70aa6b1fe3bd07421a602cb6faa0b8e6c4b
Dauren Delmukhambetov <dauren.del@gmail.com>
2021-04-20 18:38:58 +0300

    Adding controller and services

--- src/main/java/kz/toko/app/controller/ProductController.java
+++ src/main/java/kz/toko/app/controller/ProductController.java
@@ -2,15 +2,23 @@ package kz.toko.app.controller;
 
 import kz.toko.api.ProductsApi;
 import kz.toko.api.model.Product;
+import kz.toko.app.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+import static org.springframework.http.ResponseEntity.ok;
+
 @RestController
-public class ProductsController implements ProductsApi {
+@RequiredArgsConstructor
+public class ProductController implements ProductsApi {
+
+    private final ProductService productService;
+
     @Override
     public ResponseEntity<List<Product>> getProducts() {
-        return null;
+        return ok(productService.findAll());
     }
 }
