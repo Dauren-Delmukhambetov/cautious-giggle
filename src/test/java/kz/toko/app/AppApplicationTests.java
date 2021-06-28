fe9375e7073e1de66944e11022f292344d0f3d78
Dauren Delmukhambetov <dauren.del@gmail.com>
2021-06-28 19:35:56 +0300

    #18: Configure ITs to connect to Minio Playground

--- src/test/java/kz/toko/app/AppApplicationTests.java
+++ src/test/java/kz/toko/app/AppApplicationTests.java
@@ -2,8 +2,10 @@ package kz.toko.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("it")
 class AppApplicationTests {
 
 	@Test
