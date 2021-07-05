1e0444ea5482db58631138c10c1bd7ec07290d4f
Dauren Delmukhambetov <dauren.del@gmail.com>
2021-07-05 21:22:27 +0300

    #18: Added test method ordering

--- src/test/java/kz/toko/app/IntegrationTest.java
+++ src/test/java/kz/toko/app/IntegrationTest.java
@@ -1,10 +1,13 @@
 package kz.toko.app;
 
 
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("it")
+@TestMethodOrder(MethodOrderer.MethodName.class)
 public class IntegrationTest {
 }
