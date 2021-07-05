710a64599bb4761cfb955a0126169b4e26aea1c3
Dauren Delmukhambetov <dauren.del@gmail.com>
2021-07-05 21:29:54 +0300

    #18: Fixed test sql script

--- src/test/resources/db.scripts/add_one_product.sql
+++ src/test/resources/db.scripts/add_one_product.sql
@@ -1,2 +1,3 @@
 INSERT INTO products (id, name, price, created_at) OVERRIDING SYSTEM VALUE
-VALUES (1, 'Product # 1', 123.45, current_timestamp);
+VALUES (1, 'Product # 1', 123.45, current_timestamp)
+ON CONFLICT DO NOTHING;
