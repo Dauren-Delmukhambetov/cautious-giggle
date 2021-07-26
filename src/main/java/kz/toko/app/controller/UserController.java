c7397ed9ea513a09f83c674e9747a34e0748edf9
Danabekov <temirrrlan@gmail.com>
2021-07-26 13:44:25 +0600

    #26 Implement user deletion - changes after review

--- src/main/java/kz/toko/app/controller/UserController.java
+++ src/main/java/kz/toko/app/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 import java.util.List;
 
+import static org.springframework.http.ResponseEntity.noContent;
 import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
@@ -32,6 +33,6 @@ public class UserController implements UsersApi {
     @Override
     public ResponseEntity<Void> deleteUser(Long userId) {
         userService.delete(userId);
-        return ok().build();
+        return noContent().build();
     }
 }
--- src/test/resources/db.scripts/add_one_user.sql
+++ src/test/resources/db.scripts/add_one_user.sql
@@ -1,3 +1,3 @@
 INSERT INTO users (id, username, first_name, last_name, password, created_at) OVERRIDING SYSTEM VALUE
-VALUES (1, 'username', 'first_name', 'last_name', 'password', current_timestamp)
+VALUES (1, 'adam.smith', 'Adam', 'Smith', 'password', current_timestamp)
 ON CONFLICT DO NOTHING;
