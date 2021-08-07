e313eabf487290feafeae28b04bc5f103936f2de
Dauren Delmukhambetov <dauren.del@gmail.com>
2021-08-08 00:15:52 +0300

    #45: Added FileStorageService implementation with AWS S3 support

--- src/main/java/kz/toko/app/config/FileStorageConfig.java
+++ src/main/java/kz/toko/app/config/FileStorageConfig.java
@@ -6,6 +6,12 @@ import okhttp3.HttpUrl;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @Setter
 @Configuration
@@ -17,11 +23,28 @@ public class FileStorageConfig {
     private String secretKey;
 
     @Bean
-    public MinioClient client() {
+    @Profile("!aws")
+    public MinioClient minioClient() {
         return MinioClient.builder()
                         .endpoint(HttpUrl.parse(this.endpoint))
                         .credentials(this.accessKey, this.secretKey)
                         .build();
     }
+
+    @Bean
+    @Profile("aws")
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.US_EAST_1)
+                .credentialsProvider(awsCredentialsProvider())
+                .build();
+    }
+
+    @Bean
+    @Profile("aws")
+    public AwsCredentialsProvider awsCredentialsProvider() {
+        return StaticCredentialsProvider.create(AwsBasicCredentials.create(this.accessKey, this.secretKey));
+    }
+
 
 }
