fd56c639304ff09d0a57ad7a8b92ce33ff7ac277
Dauren Delmukhambetov <dauren.del@gmail.com>
2021-08-10 18:17:52 +0300

    #46: Refactor SQL scripts to be compliant with MySQL syntax

--- src/test/resources/db.scripts/add_product_to_delete.sql
+++ src/test/resources/db.scripts/add_product_to_delete.sql
@@ -1,3 +1,2 @@
-INSERT INTO products (id, name, price, created_at) OVERRIDING SYSTEM VALUE
-VALUES (2, 'Product # 2 (for deletion)', 98.765, current_timestamp)
-ON CONFLICT DO NOTHING;
+INSERT IGNORE INTO products (id, name, price, created_at)
+VALUES (2, 'Product # 2 (for deletion)', 98.765, current_timestamp);
