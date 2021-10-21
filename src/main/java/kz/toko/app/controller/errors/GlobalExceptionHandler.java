e319516f48e94206ecb85af9e8a80bed5cc0a97e
Dauren Delmukhambetov <dauren.del@gmail.com>
2021-10-21 11:27:43 +0300

    #112: Refactoring ErrorResponse to ErrorDetails and handling input validation exception (#114)

--- src/main/java/kz/toko/app/controller/errors/GlobalExceptionHandler.java
+++ src/main/java/kz/toko/app/controller/errors/GlobalExceptionHandler.java
@@ -1,21 +1,23 @@
 package kz.toko.app.controller.errors;
 
-import kz.toko.api.model.ErrorResponse;
+import kz.toko.api.model.ErrorDetails;
 import kz.toko.app.exception.EntityDeletedException;
 import kz.toko.app.exception.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
+import static java.lang.String.format;
 import static org.springframework.http.MediaType.APPLICATION_PROBLEM_JSON;
 
 @Slf4j
@@ -23,45 +25,46 @@ import static org.springframework.http.MediaType.APPLICATION_PROBLEM_JSON;
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleEntityNotFoundException(EntityNotFoundException e) {
+    public ResponseEntity<ErrorDetails> handleEntityNotFoundException(EntityNotFoundException e) {
         return buildErrorResponse(e, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(EntityDeletedException.class)
-    public ResponseEntity<ErrorResponse> handleEntityDeletedException(EntityDeletedException e) {
+    public ResponseEntity<ErrorDetails> handleEntityDeletedException(EntityDeletedException e) {
         return buildErrorResponse(e, HttpStatus.GONE);
     }
 
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ErrorResponse> handleAllUncaughtException(RuntimeException exception) {
+    public ResponseEntity<ErrorDetails> handleAllUncaughtException(RuntimeException exception) {
         log.error("Caught an exception: {}", exception.getMessage());
         return buildErrorResponse(exception, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        List<String> errorList = ex
+        final var errors = ex
                 .getBindingResult()
                 .getFieldErrors()
                 .stream()
-                .map(fieldError -> String.format("%s %s", fieldError.getField(), fieldError.getDefaultMessage()))
-                .collect(Collectors.toList());
+                .collect(Collectors.toMap(FieldError::getField,
+                        fe -> format("%s %s", fe.getField(), fe.getDefaultMessage())));
 
-        ErrorResponse response = new ErrorResponse();
-        response.setExceptionClass(ex.getClass().getName());
-        response.setMessage(ex.getLocalizedMessage());
-        response.setParams(errorList);
+        final var errorDetails = buildErrorDetails(ex, errors);
 
-        return handleExceptionInternal(ex, response, headers, status, request);
+        return handleExceptionInternal(ex, errorDetails, headers, status, request);
     }
 
-    private ResponseEntity<ErrorResponse> buildErrorResponse(Exception exception, HttpStatus httpStatus) {
-        ErrorResponse response = new ErrorResponse();
-        response.setMessage(exception.getMessage());
-        response.setExceptionClass(exception.getClass().getName());
+    private ErrorDetails buildErrorDetails(Exception exception, Map<String, String> params) {
+        return new ErrorDetails()
+                .message(exception.getMessage())
+                .exceptionClass(exception.getClass().getName())
+                .params(params);
+    }
+
+    private ResponseEntity<ErrorDetails> buildErrorResponse(Exception exception, HttpStatus httpStatus) {
         return ResponseEntity
                 .status(httpStatus)
                 .contentType(APPLICATION_PROBLEM_JSON)
-                .body(response);
+                .body(buildErrorDetails(exception, null));
     }
 }
