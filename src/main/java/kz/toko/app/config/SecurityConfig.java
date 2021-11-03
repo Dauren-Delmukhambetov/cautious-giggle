cf7ec63516b8f8ebe1cb42e6825bb8aca88ab1bb
DaurenDelmukhambetov <dauren.del@gmail.com>
2021-11-03 17:26:55 +0300

    #123: Configuring CORS

--- src/main/java/kz/toko/app/config/SecurityConfig.java
+++ src/main/java/kz/toko/app/config/SecurityConfig.java
@@ -3,10 +3,12 @@ package kz.toko.app.config;
 import kz.toko.app.filter.UserAutoSignUpFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableWebSecurity
@@ -18,7 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .cors().and()
+                .cors(configurer -> configurer.configurationSource(request -> corsConfiguration()))
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/users/**").authenticated()
@@ -29,5 +31,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .jwt();
 
         http.addFilterAfter(userAutoSignUpFilter, BearerTokenAuthenticationFilter.class);
+    }
+
+    private CorsConfiguration corsConfiguration() {
+        final var config = new CorsConfiguration().applyPermitDefaultValues();
+        config.addAllowedMethod(HttpMethod.OPTIONS.name());
+        config.addAllowedMethod(HttpMethod.DELETE.name());
+        config.addAllowedMethod(HttpMethod.PATCH.name());
+        return config;
     }
 }
